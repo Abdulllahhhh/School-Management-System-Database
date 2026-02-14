7d34daa229a79c10707f77ee74bfeaba4bd1dda5
Abdullah <abdullah.alattwi@gmail.com>
2026-02-15 02:20:21 +0300

    Update views.sql

--- sql/views.sql
+++ sql/views.sql
@@ -1,1 +1,15 @@
+USE School_Management_System;
 
+-- view for the grade report
+DROP VIEW IF EXISTS Student_Grades_Report;
+
+CREATE VIEW Student_Grades_Report AS
+SELECT
+    Students.name AS Student_Name,
+    Subjects.subject_name AS Subject,
+    Teachers.name AS Teacher_Name,
+    Grades.grade
+FROM Grades
+JOIN Students ON Grades.student_id = Students.id
+JOIN Subjects ON Grades.subject_id = Subjects.id
+JOIN Teachers ON Subjects.teacher_id = Teachers.id;
